3301f5874c697cb4202dbf7fcabb4fd3b942e405
Otto <ohotto@gmail.com>
2014-04-05 15:00:46 -0700

    added a test class

---
+++ Git Test/OTTest.h
@@ -0,0 +1,13 @@
+//
+//  OTTest.h
+//  Git Test
+//
+//  Created by Otto Ho on 4/5/14.
+//  Copyright (c) 2014 Ottotto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OTTest : NSObject
+
+@end
---
+++ Git Test/OTTest.m
@@ -0,0 +1,13 @@
+//
+//  OTTest.m
+//  Git Test
+//
+//  Created by Otto Ho on 4/5/14.
+//  Copyright (c) 2014 Ottotto. All rights reserved.
+//
+
+#import "OTTest.h"
+
+@implementation OTTest
+
+@end
